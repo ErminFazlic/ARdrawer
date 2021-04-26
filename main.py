5d76472050a201ead54d90ecde6af647846082a7
ErminFazlic <Erminfazlic97@gmail.com>
2021-04-26 09:34:35 +0200

    Added correct rgb color values

--- main.py
+++ main.py
@@ -37,15 +37,19 @@ while True:
             if indexX < 115:
                 if 0 < indexY < 120:
                     sidebar = overlayList[1][0:720, 0:115]
-                    color = (255, 0, 0)
+                    color = (255, 113, 82)
                 elif 120 < indexY < 240:
                     sidebar = overlayList[2][0:720, 0:115]
+                    color = (22, 22, 255)
                 elif 240 < indexY < 360:
                     sidebar = overlayList[3][0:720, 0:115]
+                    color = (101, 226, 201)
                 elif 360 < indexY < 480:
                     sidebar = overlayList[4][0:720, 0:115]
+                    color = (55, 128, 0)
                 elif 480 < indexY < 600:
                     sidebar = overlayList[5][0:720, 0:115]
+                    color = (255, 255, 255)
                 elif 600 < indexY < 720:
                     sidebar = overlayList[0][0:720, 0:115]
 
